1b433d89926cdbe1e195e5597fecdea9c0e650a2
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-09-08 21:11:55 +0600

    module(32-8) done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const names = ['Jony','Rony','Atha Ullah','Mobin']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name={names[0]} job='Footballer'></Person>
+        <Person name={names[1]} job='Cricketer'></Person>
+        <Person name={names[2]} job='Programmer'></Person>
+        <Person name={names[3]} job='Nutreshonist'></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props){
+  const personStyle ={
+    border:'2px solid yellow',
+    width:'400px',
+    margin:'10px'
+  }
+  return(
+    <div style={personStyle}>
+      <h2>My name: {props.name}</h2>
+      <h3>Profession: {props.job}</h3>
     </div>
   );
 }
